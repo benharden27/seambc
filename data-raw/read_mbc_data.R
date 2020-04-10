80fab3fc8c3c591a10ff1cafa34898969dcb957e
benharden27 <benharden27@gmail.com>
2020-04-10 13:42:30 -0400

    create seambc from seapacific base

---
+++ data-raw/read_mbc_data.R
@@ -0,0 +1,22 @@
+master_folder <- "~/data/SEA/MBC"
+subfolders <- list.files(master_folder, pattern = "(S|C)[0-9]{3}")
+
+for (i in 1:length(subfolders)) {
+
+  # set route folder
+  root_folder <- file.path(master_folder,subfolders[i])
+
+  # Package all data by standard routine
+  a <- sea::package_data(root_folder)
+
+  assign(subfolders[i],a)
+
+}
+
+
+usethis::use_data(C259, overwrite = TRUE)
+usethis::use_data(C266, overwrite = TRUE)
+usethis::use_data(C273, overwrite = TRUE)
+usethis::use_data(C279, overwrite = TRUE)
+# usethis::use_data(S285, overwrite = TRUE)
+
